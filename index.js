53f209e8dce200f209479139eee93e61677dfac2
EduardoM <eduardomeza540@gmail.com>
2025-06-05 19:38:33 -0700

    Gameboard module and Player factory added

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('linked correctly');
+const Gameboard = (function(){
+    let board = ['', '', '', '', '', '', '', '', ''];
+
+    return{
+        getBoard(){
+            return board;
+        },
+        logBoard(){
+            for (let i = 0; i < board.length; i += 3){
+                console.log(board.slice(i, i + 3).join(' | '));
+            }
+        }
+    };
+})();
+
+function Player(name, marker){
+    return{
+        name,
+        marker,
+        logPlayer(){
+            console.log(`Player: ${name} Marker: ${marker}`);
+        }
+    };
+}
